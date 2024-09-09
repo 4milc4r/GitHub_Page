b0f65369267df0cd8fb3d9ecc9e09b8dc50ce7dd
4milc4r <xicaaay@gmail.com>
2024-09-09 09:24:32 -0600

    N

--- github-main/script.js
+++
@@ -1,6 +0,0 @@
-const cursor = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', (e) => {
-	cursor.style.left = `${e.clientX + window.scrollX}px`;
-	cursor.style.top = `${e.clientY + window.scrollY}px`;
-});
